fc7c89abf0ddd27885a37b1bab90fd0283c46004
ghost <ahirtech.ap@gmail.com>
2025-06-12 10:37:57 +0530

    Added Final Game Logic

--- My Code/Unit - 05/script.js
+++ My Code/Unit - 05/script.js
@@ -10,8 +10,9 @@ document.querySelector('.guess').value = 15;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;  // is called state variable, cuz it is part of the application state
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -24,26 +25,25 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.number').textContent = guess;
 
             document.querySelector('body').style.backgroundColor = '#60b347';
-
             document.querySelector('.number').style.width = '30rem';
 
-        } else if (guess <= secretNumber) {
-            if ((guess === secretNumber - 1) || (guess === secretNumber - 2)) {
-                document.querySelector('.message').textContent = "Little Low !";
-            } else {
-                document.querySelector('.message').textContent = "Too Low :(";
+            if (score > highScore) {
+                highScore = score;
             }
-            if (score > 0){
-                score -= 1;
-                document.querySelector('.score').textContent = score;
-            } else {
-                document.querySelector('.message').textContent = "You Lost the Game !!";
-            }
-        } else if (guess >= secretNumber) {
-            if ((guess === secretNumber + 1) || (guess === secretNumber + 2)) {
-                document.querySelector('.message').textContent = "Little High !";
-            } else {
-                document.querySelector('.message').textContent = "Too High :(";
+            document.querySelector('.highscore').textContent = highScore;
+        } else {
+            if (guess <= secretNumber) {
+                if ((guess === secretNumber - 1) || (guess === secretNumber - 2)) {
+                    document.querySelector('.message').textContent = "Little Low !";
+                } else {
+                    document.querySelector('.message').textContent = "Too Low :(";
+                }
+            } else if (guess >= secretNumber) {
+                if ((guess === secretNumber + 1) || (guess === secretNumber + 2)) {
+                    document.querySelector('.message').textContent = "Little High !";
+                } else {
+                    document.querySelector('.message').textContent = "Too High :(";
+                }
             }
             if (score > 0){
                 score -= 1;
@@ -53,4 +53,15 @@ document.querySelector('.check').addEventListener('click', function() {
             }
         }
     }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+    document.querySelector('.message').textContent = "Start Guessing...";
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
