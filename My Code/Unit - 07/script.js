3670fe205f934f95e38830baecdcb8c9be216f57
ghost <ahirtech.ap@gmail.com>
2025-06-14 07:47:33 +0530

    Dice Game - Add major functionalities

--- My Code/Unit - 07/script.js
+++ My Code/Unit - 07/script.js
@@ -1,33 +1,76 @@
 'use strict';
 
-let scorePlayer1 = document.querySelector('#score--0').textContent;
-let scorePlayer2 = document.querySelector('#score--1').textContent;
-let currentScore1 = document.querySelector('#current--0').textContent;
-let currentScore2 = document.querySelector('#current--1').textContent;
-const btnDice = document.querySelector('.btn--roll');
-let diceValue;
+//Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
-btnDice.addEventListener('click', function(){
-    diceValue = Math.floor(Math.random() * 6) + 1;
-    // document.querySelector('.dice').src = 'new-image.png';
-   switch (diceValue) {
-       case 1:
-           document.querySelector('.dice').src = 'dice-1.png';
-           break;
-       case 2:
-           document.querySelector('.dice').src = 'dice-2.png';
-           break;
-       case 3:
-           document.querySelector('.dice').src = 'dice-3.png';
-           break;
-       case 4:
-           document.querySelector('.dice').src = 'dice-4.png';
-           break;
-       case 5:
-           document.querySelector('.dice').src = 'dice-5.png';
-           break;
-       case 6:
-           document.querySelector('.dice').src = 'dice-6.png';
-           break;
-   }
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+let playing = true;
+
+const switchPlayer = function () {
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
+
+//Rolling Dice Functionality
+btnRoll.addEventListener('click', function () {
+    if (playing) {
+        //1. Generating a random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
+
+        //2. Display dice roll
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
+
+        //3. Check for rolled 1: If true, switch to next player
+        if (dice !== 1) {
+            // Add dice to current score
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
+            // Switch to next player
+            document.getElementById(`current--${activePlayer}`).textContent = 0;
+            switchPlayer();
+        }
+    }
+});
+
+btnHold.addEventListener('click', function () {
+    if (playing) {
+        //1. Add score to active player's score
+        scores[activePlayer] += currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+        //2. Check if player's score is >= 100
+        if (scores[activePlayer] >= 20) {
+            //Finish the game
+            playing = false;
+            diceEl.classList.add('hidden');
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        }
+
+        //3. Switch to the next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        switchPlayer();
+    }
 });
