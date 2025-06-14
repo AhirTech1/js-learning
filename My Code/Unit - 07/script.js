b5c7842309660c9ddf82c7d66b5337cf4d65da8c
ghost <ahirtech.ap@gmail.com>
2025-06-14 07:55:22 +0530

    Dice Game - Final Game Completed

--- My Code/Unit - 07/script.js
+++ My Code/Unit - 07/script.js
@@ -13,16 +13,26 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores = [], currentScore, activePlayer, playing;
+
 //Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
 
-let playing = true;
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
 
 const switchPlayer = function () {
     activePlayer = activePlayer === 0 ? 1 : 0;
@@ -30,6 +40,8 @@ const switchPlayer = function () {
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
 }
+
+init();
 
 //Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
@@ -61,7 +73,7 @@ btnHold.addEventListener('click', function () {
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
         //2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             //Finish the game
             playing = false;
             diceEl.classList.add('hidden');
@@ -74,3 +86,5 @@ btnHold.addEventListener('click', function () {
         switchPlayer();
     }
 });
+
+btnNew.addEventListener('click', init);
